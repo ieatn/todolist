2b3b77ed72b510d1bea9505834de3e85a2747a95
Dennis Zhang <81874063+ieatn@users.noreply.github.com>
2022-09-24 18:41:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-let btn = document.querySelector('#btn')
-let input = document.querySelector('#input')
-let out = document.querySelector('#output')
+const output = document.querySelector('#output')
+const btn = document.querySelector('.btn')
 
 btn.addEventListener('click', () => {
-    let p = document.createElement('p')
-    p.innerHTML = input.value
-    out.appendChild(p)
-    p.classList.add('p-style')
-    input.value = ''
-    p.addEventListener('click', () => {
-        p.style.textDecoration = 'line-through'
-    })
-    p.addEventListener('dblclick', () => {
-        out.removeChild(p)
-    })
+  const todo = document.querySelector('#input').value
+  const e = document.createElement('p')
+  e.classList.add('hover')
+  e.textContent = todo
+  output.appendChild(e)
+  e.addEventListener('click', () => {
+    e.style.textDecoration = 'line-through'
+  })
+  e.addEventListener('dblclick', () => {
+    output.removeChild(e)
+  })
 })
